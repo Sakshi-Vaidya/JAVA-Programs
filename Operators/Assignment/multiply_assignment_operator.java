8fe802bd3a5979eb66782f770c6682e54d1346c8
Sakshi-Vaidya <110716058+Sakshi-Vaidya@users.noreply.github.com>
2023-10-17 23:21:02 +0530

    Create multiply_assignment_operator.java

---
+++ Operators/Assignment/multiply_assignment_operator.java
@@ -0,0 +1,10 @@
+public class AssignmentOperators {// public class declaration
+   public static void main(String[] args){ // main function declaration
+      int a,b = 2; // variable declaration and initialization
+      a = 11;
+
+      a *= b;
+      System.out.println(" *= Operator : a *= b is : " + a); // multiply assignment operator
+      
+   }
+}
