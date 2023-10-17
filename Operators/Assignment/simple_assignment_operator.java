13a613e1bc1c7dde972e859abaaf16994fccad55
Sakshi-Vaidya <110716058+Sakshi-Vaidya@users.noreply.github.com>
2023-10-17 23:08:31 +0530

    Create simple_assignment_operator.java

---
+++ Operators/Assignment/simple_assignment_operator.java
@@ -0,0 +1,7 @@
+public class AssignmentOperators {// public class declaration
+   public static void main(String[] args){ // main function declaration
+      int a,b = 2; // variable declaration and initialization
+      a = 11;
+      System.out.println(" = Operator : a = " + a); // simple assignment operator
+   }
+}
