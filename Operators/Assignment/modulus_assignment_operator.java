1cceda71f0d968f4f038f5be3bcf15ee85539949
Sakshi-Vaidya <110716058+Sakshi-Vaidya@users.noreply.github.com>
2023-10-18 16:52:22 +0530

    Create modulus_assignment_operator.java

---
+++ Operators/Assignment/modulus_assignment_operator.java
@@ -0,0 +1,9 @@
+public class AssignmentOperators {// public class declaration
+   public static void main(String[] args){ // main function declaration
+      int a,b = 2; // variable declaration and initialization
+      a = 11;
+      
+      a %= b;
+      System.out.println(" %= Operator : a %=b is : " + a); // modulus assignment operator
+   }
+}
