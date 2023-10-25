69aea3fcb2e7152f43415ebe705471007c0053dd
Sakshi-Vaidya <110716058+Sakshi-Vaidya@users.noreply.github.com>
2023-10-25 23:44:42 +0530

    Create Star_Pattern3.java

---
+++ Patterns/Star_Pattern3.java
@@ -0,0 +1,50 @@
+/*Star Pattern 5
+
+* * * * *
+
+  * * * *
+
+    * * *
+
+      * *
+
+        *  */
+
+package Patterns;
+
+import java.util.Scanner;
+
+public class Star {
+
+public static void main(String[] args) {
+
+Scanner S = new Scanner(System.in); //Input
+
+System.out.println("Enter row value ");
+
+int r = S.nextInt();
+
+for (int i = r; i >= 1; i--) { 
+
+for (int j = r; j > i; j--) { 
+
+System.out.print(" "); // Prints Blank space
+
+}
+
+for (int k = 1; k <= i; k++) {
+
+System.out.print("*"); //Prints *
+
+}
+
+System.out.println(" "); //Prints blank spaces
+
+}
+
+S.close();
+
+}
+
+}
+
